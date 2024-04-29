38f1ae4d885f10b1768e56bd0f1e1220efcf2d66
Niki <gavrilovnikita2004@gmail.com>
2024-04-30 00:19:39 +0300

    created first section

--- client/src/components/Clothes/Clothes.tsx
+++ client/src/components/Clothes/Clothes.tsx
@@ -1,8 +1,32 @@
 import React from "react";
 import styles from "./Clothes.module.scss";
+import arrow from "./images/arrow.svg";
 
 const Clothes: React.FC = () => {
-  return <section className={styles.clothes}></section>;
+  return (
+    <section className={styles.clothes}>
+      <div className="container">
+        <div className={styles.row}>
+          <div className={styles.body}>
+            <h3 className={styles.treehouse}>TREEHOUSE:</h3>
+            <h1 className={styles.title}>Little Clothes.</h1>
+            <h2 className={styles.title}>Big impact.</h2>
+            <p className={styles.description}>
+              Высококачественная детская одежда, тщательно изготовленная из экологически чистых материалов, бережно относящихся к
+              планете. Эти вневременные модели станут идеальными спутниками вашего маленького искателя приключений, куда бы его ни
+              завело воображение.
+            </p>
+            <div className={styles.buttons}>
+              <button>КУПИТЬ СЕЙЧАС</button>
+              <button>
+                <img src={arrow} alt="arrow" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Clothes;
